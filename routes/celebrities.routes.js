057cbedf17d0296acabb4565d6fe4ec5b4707268
robine81 <robine.westberg@gmail.com>
2023-04-24 17:52:08 +0200

    Iteration 3 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,19 +3,19 @@ const Celebrity = require('../models/Celebrity.model')
 
 router.get('/new-celebrity',(req, res) => {
     // console.log(req.query)
-    res.render('/new-celebrity')
+    res.render('celebrities/new-celebrity')
 })
 
-router.post('/celebrities/create', async (req, res) => {
+router.post('/create', async (req, res) => {
     console.log(req.body)
     try {
         const newCelebrity = await Celebrity.create(req.body)
-        //   response.redirect(`/celebrities/${newCelebrity._id}`)
-           response.redirect('/')
+          response.redirect(`/celebrities/${newCelebrity._id}`)
+          // response.redirect('/celebrities')
     }
     catch(error) {
         console.error('There was an error adding celeb', error)
-        res.render('/new-celebrity')
+        res.render('celebrities/new-celebrity')
     }
 
   })
