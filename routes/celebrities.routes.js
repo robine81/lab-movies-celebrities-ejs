b5246c27d8cfcae44c32836164c1050f852dff90
robine81 <robine.westberg@gmail.com>
2023-04-24 18:03:09 +0200

    Submit works, but not showing list of celebrities

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -10,14 +10,13 @@ router.post('/create', async (req, res) => {
     console.log(req.body)
     try {
         const newCelebrity = await Celebrity.create(req.body)
-          response.redirect(`/celebrities/${newCelebrity._id}`)
-          // response.redirect('/celebrities')
+        //NOT WORKING YET
+        res.redirect('/celebrities')
     }
     catch(error) {
         console.error('There was an error adding celeb', error)
-        res.render('celebrities/new-celebrity')
+        res.render('/new-celebrity')
     }
-
   })
 
 module.exports = router
